5dd2a93043e3e7045d601e48376f4a68a7431082
Manvith <manumanvith193@gmail.com>
2025-09-02 12:01:17 +0530

    Adding variables lesson

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accId = 12345
+// changing const keyword is not permitted it is fixed
+
+let accEmail = "manvith@gmail.com"
+var accPass = "234555454"
+// can also declare variables without any keyword but not recommended!
+accCity = "Chennai"
+let accState;
+
+accEmail = "karan@gmail.com"
+accPass = "20493rw834025"
+
+console.table([accId,accEmail,accPass,accState])
+/* 
+Dont use 'var' use 'let' always to maintian block and functional scope!
+*/
